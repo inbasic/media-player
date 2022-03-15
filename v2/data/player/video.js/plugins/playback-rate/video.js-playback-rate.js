9232ea7423f7ecb8c67388b16a743fb2b8261ed5
InBasic <inb.cor@gmail.com>
2022-03-15 02:12:23 -0400

    version 0.2.8

--- v2/data/player/video.js/plugins/playback-rate/video.js-playback-rate.js
+++ v2/data/player/video.js/plugins/playback-rate/video.js-playback-rate.js
@@ -13,7 +13,6 @@
         playrate = player.playbackRate();
       });
       player.on('playlistitem', function() {
-        console.log(123, playrate);
         player.playbackRate(playrate);
       });
     }
